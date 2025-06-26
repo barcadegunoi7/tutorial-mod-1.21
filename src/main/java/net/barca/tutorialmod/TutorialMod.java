2a335ea5dbffdf17af8e1ac68701df91e62c3ae6
barcadegunoi7 <alexandruionescu1998@yahoo.com>
2025-06-26 19:09:14 +0300

    Item Groups (Item & Block)

--- src/main/java/net/barca/tutorialmod/TutorialMod.java
+++ src/main/java/net/barca/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.barca.tutorialmod;
 
 import net.barca.tutorialmod.block.ModBlocks;
+import net.barca.tutorialmod.item.ModItemGroups;
 import net.barca.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
