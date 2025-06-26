5044546a624944730c36f94097e5d72acc1cedc9
barcadegunoi7 <alexandruionescu1998@yahoo.com>
2025-06-26 19:00:55 +0300

    new Blocks (Raw and ingot)

--- src/main/java/net/barca/tutorialmod/TutorialMod.java
+++ src/main/java/net/barca/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.barca.tutorialmod;
 
+import net.barca.tutorialmod.block.ModBlocks;
 import net.barca.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
